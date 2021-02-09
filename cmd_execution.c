9241e0f00c401e50c5e09b206a3185601e47cad1
Miki Rosario <chisparroja@gmail.com>
2021-02-09 22:09:45 +0100

    Bugfix: Fixed segfault when PATH contains invalid directories

--- cmd_execution.c
+++ cmd_execution.c
@@ -6,7 +6,7 @@
 /*   By: miki <miki@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 19:33:19 by mrosario          #+#    #+#             */
-/*   Updated: 2021/02/07 23:43:12 by miki             ###   ########.fr       */
+/*   Updated: 2021/02/09 22:08:48 by miki             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,7 +72,8 @@ char	*find_cmd_path(char *cmd, const char *paths, t_micli *micli)
 					if (!(ft_strcmp(dirent->d_name, cmd))) //stop if entry coincides with cmd
 						ret = generate_pathname(path_array[y], cmd, micli); //concatenate dir path with command name
 					//ft_printf("%s\n", dirent->d_name);
-			closedir(dir);
+			if (dir)
+				closedir(dir);
 			y++;
 		}			
 	
