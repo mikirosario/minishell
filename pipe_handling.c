d69ac161d14692c37a8b519d8d009c77a72d0231
Michael lucas Rosario kling <mrosario@c1r5s4.42madrid.com>
2021-02-24 21:57:42 +0100

    Version 2.32: Additional comments

--- pipe_handling.c
+++ pipe_handling.c
@@ -6,7 +6,7 @@
 /*   By: mrosario <mrosario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 12:14:45 by mrosario          #+#    #+#             */
-/*   Updated: 2021/02/24 13:33:58 by mrosario         ###   ########.fr       */
+/*   Updated: 2021/02/24 21:56:41 by mrosario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -133,14 +133,16 @@ size_t	pipe_count(const char *line, t_micli *micli)
 	line = ft_skipspaces(line);
 	while (*line)
 	{
-		escape_flag = 0;														//Status Conditions
-		if (*line == '\\' && quote_flag != 2/* && !escape_flag*/) 				//If escape is found and we aren't between single quotes.. Note that single quotes prevent escaping.
-		{
+		escape_flag = 0;														//Status Condition
+		//Note that single quotes prevent escaping.
+		if (*line == '\\' && quote_flag != 2 &&	!(quote_flag == 1				//If escape is found and the following two expressions are NOT true:
+		&& ft_memchr(DQUOTE_ESC_CHARS, *(line + 1), 3) == NULL))					//1. We are between single quotes.
+		{																			//2. We are between double quotes, but an escapable character has not been found after the escape.
 			escape_flag = 1;													//Escape flag is set.
 			line++;																//Line is incremented by one to examine escaped character.
 		}
 		if ((*line == '"' && !escape_flag && (!quote_flag || quote_flag == 1)) 	//If double quotes are found and the double quotes have not been escaped and we are either not between quotes or we are between double quotes...
-		|| (*line == '\'' && !escape_flag && (!quote_flag || quote_flag == 2)))	//OR if single quote is found and the single quote has not been escaped and we are either not between quotes or we are between single quotes.
+		|| (*line == '\'' && (quote_flag == 2 || (!escape_flag && !quote_flag))))	//OR if single quote is found and we are between single quotes or the single quote has not been escaped and we are not between quotes.
 			quote_flag = toggle_quote_flag(*line, quote_flag);					//Toggle the quote flag appropriately. Quote flag will toggle when: no quotes and quotes found or quotes and unescaped quotes of same type found.
 		if (*line == ';' && !quote_flag && !escape_flag)						//If semicolon is found and we aren't between any quotes and the semicolon hasn't been escaped
 			break ;																//Leave the while and return pipe count (we only count pipes until the next semicolon or EOL)
